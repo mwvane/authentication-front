3e3f7ddb7f2e527f8ec18086751a30e2c5255b1b
mwvane <bzishvili57@gmail.com>
2023-01-09 19:18:31 +0400

    Create home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  constructor(
+    private apiService: ApiService,
+    private router : Router
+    ){
+
+  }
+  ngOnInit(): void {
+    if(!this.apiService.isLoggedin()){
+      this.router.navigateByUrl("login")
+    }
+  }
+}
