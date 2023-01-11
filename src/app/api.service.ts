90bb03c0939af30e0bd58b3d255b83e876718955
mwvane <bzishvili57@gmail.com>
2023-01-11 11:41:24 +0400

    Udate register functionality

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { EnvironmentInjector, Injectable } from '@angular/core';
 import { User } from './user';
 import { HttpClient } from '@angular/common/http';
 @Injectable({
@@ -6,27 +6,28 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ApiService {
   constructor(private http: HttpClient) {}
+
   login(username: string, password: string) {
-    return new Promise((res,rej) => {
+    return new Promise((res, rej) => {
       const payload = { username, password };
       this.http
         .post('https://localhost:7156/api/login', payload)
         .subscribe((data) => {
-          res(data)
+          res(data);
         });
-    })
-    
+    });
   }
-  
+
   register(user: User) {
-    this.http
-      .post('https://localhost:7156/api/register', user)
-      .subscribe((res) => {
-        if(res){
-          console.log(res)
-          return res
-        }
-        return false
-      });
+    return new Promise((res, rej) => {
+      this.http
+        .post('https://localhost:7156/api/register', user)
+        .subscribe((data) => {
+          if (data) {
+            console.log(data);
+            res(data);
+          }
+        });
+    });
   }
 }
